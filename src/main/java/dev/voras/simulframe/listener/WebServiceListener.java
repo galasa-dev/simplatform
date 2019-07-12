dcee898d793a2edd6d72f7c5fef42014fae62dab
Will Yates <wyates@uk.ibm.com>
2019-07-12 11:26:56 +0100

    http fixes

--- src/main/java/dev/voras/simulframe/listener/WebServiceListener.java
+++ src/main/java/dev/voras/simulframe/listener/WebServiceListener.java
@@ -5,6 +5,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.net.Socket;
 
 public class WebServiceListener implements IListener {
@@ -15,11 +17,22 @@ public class WebServiceListener implements IListener {
 		try {
 			InputStream input = socket.getInputStream();
 			BufferedReader br = new BufferedReader(new InputStreamReader(input));
-			String data = br.readLine();
-			while(data != null) {
-				System.out.println(data);
-				data = br.readLine();
+			while(br.ready()) {
+				System.out.println(br.readLine());
 			}
+			
+			OutputStream output = socket.getOutputStream();
+			PrintStream ps = new PrintStream(output);
+			ps.println("HTTP/1.1 200 OK");
+			ps.println("Server: Simulframe 0.3.0");
+			ps.println("Content-Length: 5");
+			ps.println("Content-Type: text");
+			ps.println("Connection: Closed");
+			ps.println("");
+			ps.println("Hello");
+			ps.println("\r\n");
+			ps.flush();
+			socket.close();
 				
 		}catch(IOException e) {
 			
