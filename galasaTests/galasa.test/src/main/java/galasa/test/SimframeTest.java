17f7176ce1d4796dfc8c6b79e3502a6630008ce5
Richard Somers <richard.somers@ibm.com@richardss-mbp.hursley.uk.ibm.com>
2019-08-12 16:40:17 +0100

    Working web service and lots of clean up

--- galasaTests/galasa.test/src/main/java/galasa/test/SimframeTest.java
+++ galasaTests/galasa.test/src/main/java/galasa/test/SimframeTest.java
@@ -2,14 +2,26 @@
 package galasa.test;
 
 import dev.voras.Test;
+import dev.voras.common.artifact.ArtifactManager;
+import dev.voras.common.artifact.IArtifactManager;
+import dev.voras.common.artifact.TestBundleResourceException;
 import dev.voras.common.http.HttpClient;
+import dev.voras.common.http.HttpClientException;
+import dev.voras.common.http.IHttpClient;
 import dev.voras.common.zos.IZosImage;
 import dev.voras.common.zos.ZosImage;
 import dev.voras.common.zos3270.ITerminal;
 import dev.voras.common.zos3270.Zos3270Terminal;
-import dev.voras.common.zos3270.spi.Terminal;
+import static org.assertj.core.api.Assertions.assertThat;
 
-public class SimframeTest { 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+
+@Test
+public class SimframeTest{ 
 
     @ZosImage(imageTag="A")
     public IZosImage image;
@@ -17,82 +29,88 @@ public class SimframeTest {
     @Zos3270Terminal(imageTag="A")
     public ITerminal terminal;
 
+    @ArtifactManager
+    public IArtifactManager artifacts;
+
     @HttpClient
-    public HttpClient client;
+    public IHttpClient client;
 
     @Test
-    public void testNotNull() throws Exception{
-        if(terminal == null) {
-            throw new Exception();
-        }
-
+    public void testNotNull() {
+        //Check all objects loaded
+        assertThat(terminal).isNotNull();
+        assertThat(artifacts).isNotNull();
+        assertThat(client).isNotNull();
     }
 
     @Test
-    public void test1() {
+    public void updateAccountWebServiceTest() throws TestBundleResourceException, URISyntaxException, IOException, HttpClientException {
+            //Initial actions to get into banking applcation
+            login();
 
+            //Obain the initial balance
+            Double userBalance = getBalance("123456789");
+
+            //Set the amount be crecited and call web service
+            Double amount = 500.50;
+            HashMap<String,Object> parameters = new HashMap<String,Object>();
+            parameters.put("ACCOUNT_NUMBER", "123456789");
+            parameters.put("SORT_CODE", "000000"); //Needed or XML Not generated correctly. ++ notation could be removed from XML to be cleaner
+            parameters.put("AMOUNT", "500.50");
+
+            //Load sample request with the given parameters
+            InputStream is = artifacts.getBundleResources(this.getClass()).retrieveSkeletonFile("/resources/skeletons/testSkel.skel", parameters);
+            String textContext = artifacts.getBundleResources(this.getClass()).streamAsString(is);
+
+            //Invoke the web request
+            client.setURI(new URI("http://127.0.0.1:2080"));
+            Object response = client.postTextAsXML("updateAccount", textContext, false);
+
+            //Obtain the final balance
+            Double newUserBalance = getBalance("123456789");
+
+            //Assert that the correct amount has been credited to the account
+            assertThat(newUserBalance).isEqualTo(userBalance + amount);
+    }
+
+    private void login() {
         try {
+            //Initial log in to system
+            terminal.waitForKeyboard()
+                    .positionCursorToFieldContaining("Userid").tab()
+                    .type("boo")
+                    .positionCursorToFieldContaining("Password").tab()
+                    .type("eek")
+                    .enter().waitForKeyboard()
 
-        terminal.waitForKeyboard();
-
-        terminal.positionCursorToFieldContaining("Userid").tab()
-                .type("boo")
-                .positionCursorToFieldContaining("Password").tab()
-                .type("eek")
-                .enter()
-                .waitForKeyboard()
-
-                .pf1()
-                .waitForKeyboard()
-                .clear()
-                .waitForKeyboard()
-                .tab().type("bank")
-                .enter()
-                .waitForKeyboard()
-
-                .pf1()
-                .waitForKeyboard()
-                .positionCursorToFieldContaining("Account Number").tab()
-                .type("123456789")
-                .enter()
-                .waitForKeyboard();
-
-        Double userBalance = Double.parseDouble(terminal.retrieveFieldTextAfterFieldWithString("Balance").trim());
-        System.err.println(userBalance);
-
-        terminal.positionCursorToFieldContaining("Account Number").tab();
-        System.err.println(terminal.retrieveFieldAtCursor());
-
-        System.err.println(terminal.retrieveScreen());
-
-        terminal.pf3();
-
-        Double amount = 500.50;
-
-        sendRequest(amount, "http://127.0.0.1:2080/updateAccount");
-
-        terminal.pf1()
-                .waitForKeyboard()
-                .positionCursorToFieldContaining("Account Number").tab()
-                .type("123456789")
-                .enter()
-                .waitForKeyboard();
-
-        terminal.reportScreenWithCursor()
-                .reportScreen();
-        
-        //SOME CODE TO FIND THE BALANCE
-
-        double newUserBalance = 500.50;
-
-        //HOW EVER ASSERTION IS DONE IN GALASA(userBalance + amount == newUserBalance);
-
+            //Open banking application
+                    .pf1().waitForKeyboard()
+                    .clear().waitForKeyboard()
+                    .tab().type("bank").enter().waitForKeyboard();
         } catch(Exception e) {
-            System.out.println("Something gone wrong: " + e);
+            e.printStackTrace();
         }
     }
 
-    private String sendRequest(Double amount, String url) {
-        return null;
+    private Double getBalance(String accountNum) {
+        Double amount = 0.0;
+        try {
+            //Open account menu and enter account number
+            terminal.pf1()
+                    .waitForKeyboard()
+                    .positionCursorToFieldContaining("Account Number").tab()
+                    .type(accountNum)
+                    .enter()
+                    .waitForKeyboard();
+
+            //Retrieve balance from screen
+            amount = Double.parseDouble(terminal.retrieveFieldTextAfterFieldWithString("Balance").trim());
+
+            terminal.pf3();
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            return amount;
+        }
     }
 }
