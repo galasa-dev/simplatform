5becf9a316d01aefdeefd3e55acebb2a9416170e
Michael Baylis <Michael.Baylis@uk.ibm.com>
2019-10-14 11:21:17 +0100

    correct simplatform literals

--- galasa-simbank-tests/dev.galasa.simbank.manager/src/main/java/dev/galasa/simbank/manager/internal/SimBankTerminalImpl.java
+++ galasa-simbank-tests/dev.galasa.simbank.manager/src/main/java/dev/galasa/simbank/manager/internal/SimBankTerminalImpl.java
@@ -19,76 +19,76 @@ import dev.galasa.zos3270.spi.NetworkException;
 import dev.galasa.zos3270.spi.Zos3270TerminalImpl;
 
 public class SimBankTerminalImpl extends Zos3270TerminalImpl implements ISimBankTerminal {
-	
-	private Log logger = LogFactory.getLog(SimBankTerminalImpl.class);
-	
-	private final ICredentialsUsernamePassword credentials;
-	private final String application;
+    
+    private Log logger = LogFactory.getLog(SimBankTerminalImpl.class);
+    
+    private final ICredentialsUsernamePassword credentials;
+    private final String application;
 
-	public SimBankTerminalImpl(String id, String host, String application, ICredentialsUsernamePassword credentials, int port, boolean tls, IFramework framework)
-			throws Zos3270ManagerException, InterruptedException {
-		super(id, host, port, tls, framework);
-		this.credentials = credentials;
-		this.application = application;
-	}
+    public SimBankTerminalImpl(String id, String host, String application, ICredentialsUsernamePassword credentials, int port, boolean tls, IFramework framework)
+            throws Zos3270ManagerException, InterruptedException {
+        super(id, host, port, tls, framework);
+        this.credentials = credentials;
+        this.application = application;
+    }
 
-	@Override
-	public void gotoMainMenu() throws TimeoutException, KeyboardLockedException, DatastreamException, NetworkException, FieldNotFoundException, TextNotFoundException, ConfigurationPropertyStoreException, SimBankManagerException, InterruptedException {
-		
-		waitForKeyboard();
-		
-		while(!isTextInField("SIMBANK MAIN MENU")) {
-			if (isTextInField("SIMBANK ACCOUNT MENU")) {
-				pf3().waitForKeyboard();
-			} else if (isTextInField("SIMBANK TRANSFER MENU")) {
-				pf3().waitForKeyboard();
-			} else if (isTextInField("DFHZC2312")) {
-				enterCICSTransaction();
-			} else if (isTextInField("SIMFRAME MAIN MENU")) {
-				selectionApplication();
-			} else if (isTextInField("SIMFRAME LOGON SCREEN")) {
-				logonSessionManager();
-			} else {
-				logger.warn("Unable to determine position in application, reseting connection");
-				disconnect();
-				connect();
-			}
-		}
-	}
+    @Override
+    public void gotoMainMenu() throws TimeoutException, KeyboardLockedException, DatastreamException, NetworkException, FieldNotFoundException, TextNotFoundException, ConfigurationPropertyStoreException, SimBankManagerException, InterruptedException {
+        
+        waitForKeyboard();
+        
+        while(!isTextInField("SIMBANK MAIN MENU")) {
+            if (isTextInField("SIMBANK ACCOUNT MENU")) {
+                pf3().waitForKeyboard();
+            } else if (isTextInField("SIMBANK TRANSFER MENU")) {
+                pf3().waitForKeyboard();
+            } else if (isTextInField("DFHZC2312")) {
+                enterCICSTransaction();
+            } else if (isTextInField("SIMPLATFORM MAIN MENU")) {
+                selectionApplication();
+            } else if (isTextInField("SIMPLATFORM LOGON SCREEN")) {
+                logonSessionManager();
+            } else {
+                logger.warn("Unable to determine position in application, reseting connection");
+                disconnect();
+                connect();
+            }
+        }
+    }
 
-	private void logonSessionManager() throws DatastreamException, TimeoutException, KeyboardLockedException, NetworkException, FieldNotFoundException, TextNotFoundException, ConfigurationPropertyStoreException, SimBankManagerException, InterruptedException {
-		verifyTextInField(("SIMFRAME LOGON SCREEN"))
-		.positionCursorToFieldContaining("Userid")
-		.tab()
-		.type(credentials.getUsername())
-		.positionCursorToFieldContaining("Password")
-		.tab()
-		.type(credentials.getPassword())
-		.enter()
-		.waitForKeyboard();
-		
-		selectionApplication();
-	}
+    private void logonSessionManager() throws DatastreamException, TimeoutException, KeyboardLockedException, NetworkException, FieldNotFoundException, TextNotFoundException, ConfigurationPropertyStoreException, SimBankManagerException, InterruptedException {
+        verifyTextInField(("SIMPLATFORM LOGON SCREEN"))
+        .positionCursorToFieldContaining("Userid")
+        .tab()
+        .type(credentials.getUsername())
+        .positionCursorToFieldContaining("Password")
+        .tab()
+        .type(credentials.getPassword())
+        .enter()
+        .waitForKeyboard();
+        
+        selectionApplication();
+    }
 
-	private void selectionApplication() throws DatastreamException, TimeoutException, KeyboardLockedException, NetworkException, FieldNotFoundException, TextNotFoundException, ConfigurationPropertyStoreException, SimBankManagerException, InterruptedException {
-		verifyTextInField("SIMFRAME MAIN MENU")
-		.positionCursorToFieldContaining("===>")
-		.tab()
-		.type(SimBankApplicationName.get(this.application))
-		.enter()
-		.waitForKeyboard();
-		
-		enterCICSTransaction();
-	}
+    private void selectionApplication() throws DatastreamException, TimeoutException, KeyboardLockedException, NetworkException, FieldNotFoundException, TextNotFoundException, ConfigurationPropertyStoreException, SimBankManagerException, InterruptedException {
+        verifyTextInField("SIMPLATFORM MAIN MENU")
+        .positionCursorToFieldContaining("===>")
+        .tab()
+        .type(SimBankApplicationName.get(this.application))
+        .enter()
+        .waitForKeyboard();
+        
+        enterCICSTransaction();
+    }
 
-	private void enterCICSTransaction() throws TimeoutException, KeyboardLockedException, NetworkException, TextNotFoundException, FieldNotFoundException, InterruptedException {
-		verifyTextInField("DFHZC2312")
-		.clear()
-		.waitForKeyboard()
-		.tab()
-		.type("bank")
-		.enter()
-		.waitForKeyboard();
-	}
+    private void enterCICSTransaction() throws TimeoutException, KeyboardLockedException, NetworkException, TextNotFoundException, FieldNotFoundException, InterruptedException {
+        verifyTextInField("DFHZC2312")
+        .clear()
+        .waitForKeyboard()
+        .tab()
+        .type("bank")
+        .enter()
+        .waitForKeyboard();
+    }
 
 }
