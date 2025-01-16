fd62f66091199fd5a8bc29a84ff06a7b39b8b79e
Jade Carino <carino_jade@yahoo.co.uk>
2025-01-16 21:59:19 +0000

    Uplift development version to 0.40.0
    Signed-off-by: Jade Carino <carino_jade@yahoo.co.uk>

--- run-locally.sh
+++ run-locally.sh
@@ -111,7 +111,7 @@ fi
 # Main logic.
 #-----------------------------------------------------------------------------------------                   
 
-SIMBANK_VERSION="0.39.0"
+SIMBANK_VERSION="0.40.0"
 
 function run_server {
     h1 "Running Simbank back-end server application (version ${SIMBANK_VERSION}) ..."
