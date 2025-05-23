224d3a9d6ed14a3a1a6ac19ea277b23cfabcdb49
Jade Carino <carino_jade@yahoo.co.uk>
2025-05-23 10:50:18 +0100

    Bash script for running the Simbank app and all IVTs locally, GH workflow to run the SimBankIVT locally in a workflow, updates to the .secrets.baseline
    Signed-off-by: Jade Carino <carino_jade@yahoo.co.uk>

--- test-locally.sh
+++ test-locally.sh
@@ -7,7 +7,7 @@
 #
 #-----------------------------------------------------------------------------------------                   
 #
-# Objectives: Build this repository code locally.
+# Objectives: Run the Simbank tests locally.
 # 
 #-----------------------------------------------------------------------------------------                   
 
@@ -92,6 +92,89 @@ function checkGalasaCtlAvailable {
     fi
 }
 
+function checkSimBankTestAreBuilt {
+    SIMBANK_TESTS_PATH=$(ls ~/.m2/repository/dev/galasa/dev.galasa.simbank.tests/${SIMBANK_VERSION}/dev.galasa.simbank.tests-${SIMBANK_VERSION}.jar 2>/dev/null)
+
+    if [ -f "$SIMBANK_TESTS_PATH" ]; then
+        info "Found dev.galasa.simbank.tests in local Maven repo: $SIMBANK_TESTS_PATH"
+    else
+        error "dev.galasa.simbank.tests not found in local Maven repo." >&2
+        exit 1
+    fi
+}
+
+function checkSimBankAppIsBuilt {
+    SIMBANK_APP_PATH=$(ls ~/.m2/repository/dev/galasa/galasa-simplatform/${SIMBANK_VERSION}/galasa-simplatform-${SIMBANK_VERSION}.jar 2>/dev/null)
+
+    if [ -f "$SIMBANK_APP_PATH" ]; then
+        info "Found galasa-simplatform in local Maven repo: $SIMBANK_APP_PATH"
+    else
+        error "galasa-simplatform not found in local Maven repo." >&2
+        exit 1
+    fi
+}
+
+#-----------------------------------------------------------------------------------------                   
+# Test methods.
+#-----------------------------------------------------------------------------------------                   
+function runSimBankIVT {
+    cmd="galasactl runs submit local \
+    --obr mvn:dev.galasa/dev.galasa.simbank.obr/${TEST_OBR_VERSION}/obr \
+    --obr mvn:dev.galasa/dev.galasa.uber.obr/${TEST_OBR_VERSION}/obr \
+    --class dev.galasa.simbank.tests/dev.galasa.simbank.tests.SimBankIVT \
+    --log ${BASEDIR}/temp/simbank-test-log.txt"
+
+    info "Command is ${cmd}"
+
+    $cmd
+    rc=$?
+    if [[ "${rc}" != "0" ]]; then
+        error "Command to run tests failed. rc=${rc}. Log is at ${BASEDIR}/temp/log.txt"
+        exit 1
+    fi
+
+    success "Ran SimBankIVT test OK"
+}
+
+function runBasicAccountCreditTest {
+    cmd="galasactl runs submit local \
+    --obr mvn:dev.galasa/dev.galasa.simbank.obr/${TEST_OBR_VERSION}/obr \
+    --obr mvn:dev.galasa/dev.galasa.uber.obr/${TEST_OBR_VERSION}/obr \
+    --class dev.galasa.simbank.tests/dev.galasa.simbank.tests.BasicAccountCreditTest \
+    --log ${BASEDIR}/temp/simbank-test-log.txt"
+
+    info "Command is ${cmd}"
+
+    $cmd
+    rc=$?
+    if [[ "${rc}" != "0" ]]; then
+        error "Command to run tests failed. rc=${rc}. Log is at ${BASEDIR}/temp/log.txt"
+        exit 1
+    fi
+
+    success "Ran BasicAccountCreditTest test OK"
+}
+
+function runProvisionedAccountCreditTests {
+    cmd="galasactl runs submit local \
+    --obr mvn:dev.galasa/dev.galasa.simbank.obr/${TEST_OBR_VERSION}/obr \
+    --obr mvn:dev.galasa/dev.galasa.uber.obr/${TEST_OBR_VERSION}/obr \
+    --class dev.galasa.simbank.tests/dev.galasa.simbank.tests.ProvisionedAccountCreditTests \
+    --log ${BASEDIR}/temp/simbank-test-log.txt"
+
+    info "Command is ${cmd}"
+
+    $cmd
+    rc=$?
+    if [[ "${rc}" != "0" ]]; then
+        error "Command to run tests failed. rc=${rc}. Log is at ${BASEDIR}/temp/log.txt"
+        exit 1
+    fi
+
+    success "Ran ProvisionedAccountCreditTests test OK"
+}
+
+
 #-----------------------------------------------------------------------------------------                   
 # Main logic.
 #-----------------------------------------------------------------------------------------                   
@@ -103,24 +186,23 @@ SIMBANK_VERSION="0.42.0"
 h1 "Running Simbank application tests"
 
 checkGalasaCtlAvailable
+checkSimBankTestAreBuilt
+checkSimBankAppIsBuilt
+
+# Start the Simbank back-end server application in a background process...
+h1 "Running Simbank back-end server application (version ${SIMBANK_VERSION}) ..."
+
+java -jar ~/.m2/repository/dev/galasa/galasa-simplatform/${SIMBANK_VERSION}/galasa-simplatform-${SIMBANK_VERSION}.jar > ${BASEDIR}/temp/simbank-app-log.txt 2>&1 &
+
+success "Simbank application started OK."
 
 TEST_OBR_VERSION="0.42.0"
 
 mkdir -p ${BASEDIR}/temp
 cd ${BASEDIR}/temp
 
-cmd="galasactl runs submit local \
---obr mvn:dev.galasa/dev.galasa.simbank.obr/${TEST_OBR_VERSION}/obr \
---class dev.galasa.simbank.tests/dev.galasa.simbank.tests.SIMBANKIVT \
---log ${BASEDIR}/temp/log.txt"
+runSimBankIVT
+runBasicAccountCreditTest
+runProvisionedAccountCreditTests
 
-info "Command is ${cmd}"
-
-$cmd
-rc=$?
-if [[ "${rc}" != "0" ]]; then
-    error "Command to run tests failed. rc=${rc}. Log is at ${BASEDIR}/temp/log.txt"
-    exit 1
-fi
-
-success "Ran Simbank test OK"
+success "All SimBank tests Passed."
